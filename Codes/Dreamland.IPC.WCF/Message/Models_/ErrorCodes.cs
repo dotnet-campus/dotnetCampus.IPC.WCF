17b6bc1f70adde859cf6b9b7d9ce60e3d6e68f50
Firito <yuzhefirito@gmai.com>
2019-12-18 19:23:33 +0800

    [modify] 细化错误消息；

--- Codes/Dreamland.IPC.WCF/Message/Models_/ErrorCodes.cs
+++ Codes/Dreamland.IPC.WCF/Message/Models_/ErrorCodes.cs
@@ -40,13 +40,13 @@ namespace Dreamland.IPC.WCF.Message
         /// <summary>
         /// 消息处理查找消息对应的处理方法失败
         /// </summary>
-        [Log("响应端消息处理查找消息对应的处理方法失败，可能原因：1.未注册该消息的处理服务；2.请求端请求方法签名和响应端委托方法签名不一致（如：一端调用异步方法，另一端使用异步委托）")]
+        [Log("响应端消息处理查找消息Id（'RequestMessage.Id'）对应的处理方法失败，可能原因：1.未注册该消息的处理服务；2.请求端请求方法签名和响应端委托方法签名不一致（如：一端调用异步方法，另一端使用异步委托）")]
         GetRequestMessageListenerFailed = 201,
 
         /// <summary>
         /// 服务端与客户端发送消息失败，未找到对应的客户端
         /// </summary>
-        [Log("服务端与客户端发送消息失败，未找到对应的客户端")]
+        [Log("服务端与客户端发送消息失败，未找到消息目标（'RequestMessage.Destination'）对应的客户端")]
         FindClientFailed = 202,
     }
 }
