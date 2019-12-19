6393e1baeb64ebf91b5157dad644a4e17d46da1a
Firito <yuzhefirito@gmai.com>
2019-12-19 12:21:29 +0800

    [modify] 重要更新：增加通过反射和特性，更方便的注册消息处理方法；

---
+++ Codes/Dreamland.IPC.WCF/Attributes_/MessageHandlerAttribute.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dreamland.IPC.WCF
+{
+    /// <summary>
+    /// 消息处理类型
+    /// </summary>
+    public enum MessageHandlerType
+    {
+        /// <summary>
+        /// Request
+        /// </summary>
+        Request,
+
+        /// <summary>
+        /// RequestAsync
+        /// </summary>
+        RequestAsync,
+
+        /// <summary>
+        /// Notify
+        /// </summary>
+        Notify
+    }
+
+    /// <summary>
+    /// 消息处理
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Method)]
+    public class MessageHandlerAttribute : Attribute
+    {
+        /// <summary>
+        /// 消息Id
+        /// </summary>
+        public string MessageId { get; }
+
+        /// <summary>
+        /// 消息类型
+        /// </summary>
+        public MessageHandlerType Type { get; }
+
+        /// <summary>
+        /// 构造
+        /// </summary>
+        /// <param name="messageId"></param>
+        /// <param name="type"></param>
+        public MessageHandlerAttribute(string messageId, MessageHandlerType type)
+        {
+            MessageId = messageId;
+            Type = type;
+        }
+    }
+}
---
+++ Codes/Dreamland.IPC.WCF/Extensions/MessageHandler_/MessageHandlerFactory.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Reflection;
+using System.Threading.Tasks;
+using Dreamland.IPC.WCF.Message;
+
+namespace Dreamland.IPC.WCF.Extensions
+{/// <summary>
+ /// 消息处理器工厂
+ /// </summary>
+    public static class MessageHandlerFactory
+    {
+        /// <summary>
+        /// 通过特性自动注册Handler
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="messageHandler"></param>
+        /// <param name="instance"></param>
+        public static void RegisterHandlers<T>(this IMessageHandler messageHandler, T instance) where T : class
+        {
+            var methods = typeof(T).GetMethods();
+            foreach (var methodInfo in methods)
+            {
+                try
+                {
+                    var attribute = methodInfo.GetCustomAttribute<MessageHandlerAttribute>();
+                    if (attribute != null)
+                    {
+                        switch (attribute.Type)
+                        {
+                            case MessageHandlerType.Notify:
+                                messageHandler.TryAddMessageListener(attribute.MessageId,
+                                    message => NotifyAction(instance, methodInfo, message));
+                                break;
+                            case MessageHandlerType.Request:
+                                messageHandler.TryAddMessageListener(attribute.MessageId,
+                                    message => RequestFunc(instance, methodInfo, message));
+                                break;
+                            case MessageHandlerType.RequestAsync:
+                                messageHandler.TryAddMessageListener(attribute.MessageId,
+                                    message => RequestAsyncFunc(instance, methodInfo, message));
+                                break;
+                        }
+                    }
+                }
+                catch (Exception)
+                {
+                    //
+                }
+            }
+        }
+
+        /// <summary>
+        /// 通过反射和特性自动注册Handler
+        /// 注：使用该方法需要确保<see cref="Type"/>对应的类拥有默认构造函数
+        /// </summary>
+        /// <param name="messageHandler"></param>
+        /// <param name="type"></param>
+        public static void RegisterHandlersFromType(this IMessageHandler messageHandler, Type type)
+        {
+            var obj = Activator.CreateInstance(type);
+            messageHandler.RegisterHandlers(obj);
+        }
+
+        private static void NotifyAction(object instance, MethodInfo methodInfo, NotifyMessage arg)
+        {
+            methodInfo.Invoke(instance, new object[] { arg });
+        }
+
+        private static ResponseMessage RequestFunc(object instance, MethodInfo methodInfo, RequestMessage arg)
+        {
+            return (ResponseMessage)methodInfo.Invoke(instance, new object[] { arg });
+        }
+
+        private static Task<ResponseMessage> RequestAsyncFunc(object instance, MethodInfo methodInfo, RequestMessage arg)
+        {
+            return (Task<ResponseMessage>)methodInfo.Invoke(instance, new object[] { arg });
+        }
+    }
+}
