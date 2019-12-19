636bbb38c5339c134e8c7efa335dc8c56722f381
Firito <yuzhefirito@gmai.com>
2019-12-19 10:57:10 +0800

    [modify] 删除无效方法；

--- Codes/Dreamland.IPC.WCF/Attributes_/AttributeHelper.cs
+++ Codes/Dreamland.IPC.WCF/Attributes_/AttributeHelper.cs
@@ -10,25 +10,6 @@ namespace Dreamland.IPC.WCF
     internal static class AttributeHelper
     {
         /// <summary>
-        /// 获取对象上的指定特性
-        /// </summary>
-        /// <param name="classType"></param>
-        /// <param name="fieldName"></param>
-        /// <returns></returns>
-        public static TResult GetCustomAttribute<TResult>(this Type classType, string fieldName) where TResult : Attribute
-        {
-            foreach (var fieldInfo in classType.GetFields())
-            {
-                if (fieldInfo.Name == fieldName)
-                {
-                    return fieldInfo.GetCustomAttribute(typeof(TResult), false) as TResult;
-                }
-            }
-
-            return null;
-        }
-
-        /// <summary>
         /// 获取枚举值上的指定特性
         /// </summary>
         /// <typeparam name="TResult"></typeparam>
