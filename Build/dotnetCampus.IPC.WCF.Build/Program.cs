3d9ae3f0b85e74435a72faf04f841f51543aff39
lindexi <lindexi_gd@163.com>
2020-01-01 16:34:25 +0800

    添加打包

---
+++ Build/dotnetCampus.IPC.WCF.Build/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using dotnetCampus.DotNETBuild;
+
+namespace dotnetCampus.IPC.WCF.Build
+{
+    class Program : Compiler
+    {
+        static void Main(string[] args)
+        {
+            var program = new Program();
+            program.Compile();
+        }
+
+        public override void Compile()
+        {
+            MsBuild.Build();
+        }
+    }
+}
